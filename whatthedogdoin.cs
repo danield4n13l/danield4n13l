90659f413702b32b8521e217586fd9a05b7d3f66
danield4n13l <danipupak2@gmail.com>
2022-07-21 03:05:56 +0200

    i dont even know bruh

---
+++ whatthedogdoin.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Name
+{
+    class whatthedogdoin
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("What the dog doin?");
+            Console.ReadLine();
+        }
+    }
+}
